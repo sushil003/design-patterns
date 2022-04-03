b0cf787753a959ab947a3da9936215b749469bc5
Sushil <sushilzende1@gmail.com>
2022-04-03 14:09:24 -0400

    initial commit

---
+++ src/com/design/patterns/creational/Test.java
@@ -0,0 +1,11 @@
+package com.design.patterns.creational;
+
+/**
+ * @author Sushil
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
